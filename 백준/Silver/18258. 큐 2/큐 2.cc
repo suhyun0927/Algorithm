459f91fe393c3fafff61483674de509b21997673
강수현 <127826893+suhyun0927@users.noreply.github.com>
2024-10-26 17:53:37 +0900

    [Silver IV] Title: 큐 2, Time: 284 ms, Memory: 10420 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+queue<int> q;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        char input[6];
+        cin >> input;
+
+        if (strcmp(input, "push") == 0) {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        else if (strcmp(input, "pop") == 0) {
+            if (q.empty()) {
+                cout << "-1\n";
+                continue;
+            }
+            cout << q.front() << "\n";
+            q.pop();
+        }
+        else if (strcmp(input, "size") == 0) {
+            cout << q.size() << "\n";
+        }
+        else if (strcmp(input, "empty") == 0) {
+            cout << q.empty() <<"\n";
+        }
+        else if (strcmp(input, "front") == 0) {
+            if (q.empty()) cout << "-1\n";
+            else cout << q.front() << "\n";
+        }
+        else if (strcmp(input, "back") == 0) {
+            if (q.empty()) cout << "-1\n";
+            else cout << q.back() << "\n";
+        }
+    }
+
+    return 0;
+}
