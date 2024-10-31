b3559f01c0eebbb7ed6fbaec9b948cf14f9eb4b7
강수현 <127826893+suhyun0927@users.noreply.github.com>
2024-11-01 01:03:50 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+
+int arr[100001];
+int n, k;
+int Max;
+queue<int>q;
+
+int main() {
+	// input
+	cin >> n >> k;
+	Max = abs(k - n);
+
+	// init
+	for (int i = 0;i < 100001;i++) arr[i] = 21e8;
+
+	// bfs
+	arr[n] = 0;
+	q.push(n);
+
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+
+		// 걷기
+		int next;
+		for (int i = 0;i < 3;i++) {
+			if(i==0) next = now + 1;
+			if(i==1) next = now - 1;
+			if(i==2) next = now * 2;
+
+
+			if (next < 0 || next >= 100001) continue;
+			if (arr[next] <= arr[now] + 1) continue;
+			if (arr[now] + 1 > Max) continue;
+
+			arr[next] = arr[now] + 1;
+			q.push(next);
+		}
+	}
+
+	cout << arr[k];
+
+	return 0;
+}
