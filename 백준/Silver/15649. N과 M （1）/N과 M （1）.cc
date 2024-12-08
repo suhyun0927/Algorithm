933cb588c9206e563db5e0f4bd02ce9122b29d2d
강수현 <127826893+suhyun0927@users.noreply.github.com>
2024-12-08 18:49:00 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+using namespace std;
+
+int N, M;
+int flag[9]; // 레벨따라 값 넣어놓을거
+int vis[9]; // 넣었는지 여부
+
+void run(int level) {
+	if (level == M) {
+		for (int i = 0;i < M;i++) cout << flag[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1;i <= N;i++) {
+		if (vis[i] == 1) continue;
+
+		flag[level] = i;
+		vis[i] = 1;
+		run(level + 1);
+		vis[i] = 0;
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N >> M;
+
+	run(0);
+}
