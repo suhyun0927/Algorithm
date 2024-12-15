c4397d2ad8d074144184f235255db64da0c159d1
강수현 <127826893+suhyun0927@users.noreply.github.com>
2024-12-15 19:37:19 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+struct Node {
+	int x;
+	int y;
+
+	bool operator<(Node r) const {
+		if (y == r.y) return x > r.x;
+		return y > r.y;
+	}
+};
+
+int N;
+priority_queue<Node> pq;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);  
+	cout.tie(0);
+
+	cin >> N;
+
+	for (int i = 0;i < N;i++) {
+		int x, y;
+		cin >> x >> y;
+		
+		pq.push({ x, y });
+	}
+
+	while (!pq.empty()) {
+		Node now = pq.top();
+		pq.pop();
+
+		cout << now.x << " " << now.y << "\n";
+	}
+
+	return 0;
+}
