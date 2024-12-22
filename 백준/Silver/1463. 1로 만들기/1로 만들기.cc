f7d9661af86af431802ed2838a67aa4950ccc844
강수현 <127826893+suhyun0927@users.noreply.github.com>
2024-12-22 23:44:25 +0900

    [Silver III] Title: 1로 만들기, Time: 32 ms, Memory: 37056 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+int X;
+int dp[1000001];
+
+int go(int n) {
+	if (dp[n] != -1) return dp[n];
+
+	dp[n] = go(n - 1) + 1;
+
+	if (n % 3 == 0) dp[n] = min(dp[n], go(n / 3) + 1);
+
+	if (n % 2 == 0) dp[n] = min(dp[n], go(n / 2) + 1);
+
+	return dp[n];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> X;
+	
+	// init
+	for (int i = 0; i <= X; i++) dp[i] = -1;
+	dp[1] = 0;
+
+	cout << go(X);
+
+	return 0;
+}
