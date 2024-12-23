2585cbbea9add50a4ca69c34b389a49f315c1ad4
강수현 <127826893+suhyun0927@users.noreply.github.com>
2024-12-23 22:42:24 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+int T, N;
+int arr[12];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);  
+	cout.tie(0);
+	
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
+	for (int i = 4;i <= 11; i++) arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+
+	cin >> T;
+
+	for (int i = 0;i < T;i++) {
+		cin >> N;
+		cout << arr[N] << "\n";
+	}
+
+	return 0;
+}
