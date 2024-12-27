c32bfcfd12bc3df9211c9311dd4ac2be71ca91c7
강수현 <127826893+suhyun0927@users.noreply.github.com>
2024-12-27 11:10:07 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+int N;
+int dp[1000];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);  
+	cout.tie(0);
+	
+	cin >> N;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3;i <= N;i++) {
+		dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+	}
+
+	cout << dp[N];
+
+	return 0;
+}
